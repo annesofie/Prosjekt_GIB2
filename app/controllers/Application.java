7ad8d444157d2008af6d8a4d950b5c794c0d5802
Anne Sofie <aserichs@stud.ntnu.no>
2015-03-06 15:10:13 +0100

    tester ut koden, skal ha mathilde sin siste push

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,22 @@
 package controllers;
 
+import models.Vare;
+
 import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
+import java.util.List;
+
+
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+
+        List<Vare> alleVarer = Vare.find.all();
+
+        return ok(index.render("Finn den varen"));
     }
 
 }
