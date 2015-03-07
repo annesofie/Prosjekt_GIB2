d5b9d1e423c26fd04b43550fbb7723aa79532ff2
Anne Sofie <aserichs@stud.ntnu.no>
2015-03-07 16:20:17 +0100

    kategoriene henter varene sine fra databasen, endringer i html, css og application

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,10 +3,12 @@ package controllers;
 import models.Vare;
 
 import play.*;
+import play.api.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,8 +17,33 @@ public class Application extends Controller {
     public static Result index() {
 
         List<Vare> alleVarer = Vare.find.all();
+        List<Vare> elektro = new ArrayList<>();
+        List<Vare> fritid = new ArrayList<>();
+        List<Vare> jernvare = new ArrayList<>();
+        List<Vare> multimedia = new ArrayList<>();
+        List<Vare> hjem = new ArrayList<>();
 
-        return ok(index.render("Finn den varen",alleVarer));
+        for(Vare vare:alleVarer){
+
+            if(vare.kategori.equals("elektro")){
+                elektro.add(vare);
+            }
+            if(vare.kategori.equals("fritid")){
+                fritid.add(vare);
+            }
+            if(vare.kategori.equals("jernvare")){
+                jernvare.add(vare);
+            }
+            if(vare.kategori.equals("multimedia")){
+                multimedia.add(vare);
+            }
+            if(vare.kategori.equals("hjem")){
+                hjem.add(vare);
+            }
+
+        }
+        return ok(index.render("Finn den varen", alleVarer, elektro, fritid, hjem, jernvare, multimedia));
     }
+
 
 }
