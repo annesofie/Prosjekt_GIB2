d447dabc8bc2e2cdf7da1b4d03dac7476234bdfd
annesofie <aserichs@stud.ntnu.no>
2015-03-07 16:46:09 +0100

    Update User.java

--- app/models/User.java
+++ app/models/User.java
@@ -5,6 +5,7 @@ package models;
  */
 import play.db.ebean.*;
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class User extends Model  {
@@ -12,15 +13,16 @@ public class User extends Model  {
     @Id
     public String email;
     public String password;
+    public static List<Vare> handleliste;
+
 
     public User(String email, String password){
         this.email = email;
         this.password = password;
     }
 
-    public static Finder<String,User> find = new Model.Finder(
-            String.class, User.class
-    );
+    public static Finder<String,User> find = new Model.Finder(String.class, User.class);
+
 
     public static User authenticate(String email, String password){
         return find.where()
@@ -29,6 +31,23 @@ public class User extends Model  {
 
     }
 
+    public static User createUser(String email, String passord){
+        User user = new User(email, passord);
+        handleliste=null;
+        user.save();
+        return user;
+    }
+
+    public static void addToHandleliste(Vare vare, String email){
+        //må jeg spesifiserer hvem sin handleliste den skal legges i?
+        handleliste.add(vare);
+
+    }
+
+
+    public static void deleteFromHandleliste(Vare vare, String email){
+        handleliste.remove(vare);
+    }
 
 
 }
