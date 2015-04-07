d85a7a126dab2628806b00769d6061fc707f9c0b
gib2 <mathilde.orstavik@gmail.com>
2015-04-07 13:08:55 +0200

    flytte metode

--- app/models/SortedShoppingList.java
+++ app/models/SortedShoppingList.java
@@ -1,10 +1,27 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by mathilde on 07/04/15.
  */
 public class SortedShoppingList {
 
+    public static List<Vare> sortShoppingList() {
+        Path path = new Path(1,100); //legg inn rotnodenummer og kassenodenummer i input
+        List<Vertex>p=path.finalPath;
+        List<Vare> sortedShoppingList = new ArrayList<Vare>();
 
+        for (Vertex vertex: p) {
+            for (Vare vare: User.handleliste) {
+                if (vare.vertex == vertex) {
+                    sortedShoppingList.add(vare);
+                }
+            }
+        }
+
+        return sortedShoppingList;
+    }
 
 }
