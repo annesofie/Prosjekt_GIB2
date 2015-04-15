e286c2252d137133feec06966580098d20cf3945
dingyling <simen_hel@hotmail.com>
2015-04-15 11:17:34 +0200

    Kartfiksing

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -41,27 +41,47 @@ var init = function() {
 
     L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
- /*   var vertex1 = L.marker([60, 317]).bindPopup("1").addTo(map);
-    var vertex2 = L.marker([60, 260]).bindPopup("2").addTo(map);
-    var vertex3 = L.marker([115, 260]).bindPopup("3").addTo(map);
-    var vertex4 = L.marker([170, 260]).bindPopup("4").addTo(map);
-    var vertex5 = L.marker([225, 260]).bindPopup("5").addTo(map);
-    var vertex6 = L.marker([288, 260]).bindPopup("6").addTo(map);
-    var vertex7 = L.marker([351, 260]).bindPopup("7").addTo(map);
-    var vertex8 = L.marker([415, 260]).bindPopup("8").addTo(map);
-    var vertex9 = L.marker([415, 204]).bindPopup("9").addTo(map);
-    var vertex10 = L.marker([415, 147]).bindPopup("10").addTo(map);
-    var vertex11 = L.marker([415, 90]).bindPopup("11").addTo(map);
-    var vertex12 = L.marker([351, 90]).bindPopup("12").addTo(map);
-    var vertex13 = L.marker([288, 90]).bindPopup("13").addTo(map);
-    var vertex14 = L.marker([225, 90]).bindPopup("14").addTo(map);
-    var vertex15 = L.marker([170, 90]).bindPopup("15").addTo(map);
-    var vertex16 = L.marker([115, 90]).bindPopup("16").addTo(map);
-    var vertex17 = L.marker([60, 147]).bindPopup("17").addTo(map);
-    var vertex18 = L.marker([60, 204]).bindPopup("18").addTo(map);
-    var vertex19 = L.marker([225, 147]).bindPopup("19").addTo(map);
-    var vertex20 = L.marker([225, 204]).bindPopup("20").addTo(map);*/
+    $('.søk-knapp').click(function() {
+        var inngang = L.marker([60, 317]).bindPopup("Inngang").addTo(map);
+        var vertex2 = L.marker([60, 260]).bindPopup("2").addTo(map);
+        var vertex3 = L.marker([115, 260]).bindPopup("3").addTo(map);
+        var vertex4 = L.marker([170, 260]).bindPopup("4").addTo(map);
+        var vertex5 = L.marker([225, 260]).bindPopup("5").addTo(map);
+        var vertex6 = L.marker([288, 260]).bindPopup("6").addTo(map);
+        var vertex7 = L.marker([351, 260]).bindPopup("7").addTo(map);
+        var vertex8 = L.marker([415, 260]).bindPopup("8").addTo(map);
+        var vertex9 = L.marker([415, 204]).bindPopup("9").addTo(map);
+        var vertex10 = L.marker([415, 147]).bindPopup("10").addTo(map);
+        var vertex11 = L.marker([415, 90]).bindPopup("11").addTo(map);
+        var vertex12 = L.marker([351, 90]).bindPopup("12").addTo(map);
+        var vertex13 = L.marker([288, 90]).bindPopup("13").addTo(map);
+        var vertex14 = L.marker([225, 90]).bindPopup("14").addTo(map);
+        var vertex15 = L.marker([170, 90]).bindPopup("15").addTo(map);
+        var vertex16 = L.marker([115, 90]).bindPopup("16").addTo(map);
+        var vertex17 = L.marker([60, 147]).bindPopup("17").addTo(map);
+        var vertex18 = L.marker([60, 204]).bindPopup("18").addTo(map);
+        var vertex19 = L.marker([225, 147]).bindPopup("19").addTo(map);
+        var vertex20 = L.marker([225, 204]).bindPopup("20").addTo(map);
+    });
 
+    var inngangIcon = L.icon({
+        iconUrl: 'http://goo.gl/TKKxdJ',
+
+        iconSize:     [50, 50], // size of the icon
+        iconAnchor:   [25, 50], // point of the icon which will correspond to marker's location
+        popupAnchor:  [0, -50] // point from which the popup should open relative to the iconAnchor
+    });
+
+    var kasseIcon = L.icon({
+        iconUrl: 'http://goo.gl/eRJ0jt',
+
+        iconSize:     [50, 50], // size of the icon
+        iconAnchor:   [25, 50], // point of the icon which will correspond to marker's location
+        popupAnchor:  [0, -50] // point from which the popup should open relative to the iconAnchor
+    });
+
+
+    if (window.location.pathname == '/shoppingPath') {
         var latlngs = Array();
         appRoutes.controllers.Application.getTargetVertices().ajax({
             success: function (data) {
@@ -74,6 +94,11 @@ var init = function() {
                 var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
             }
         });
+    }
+
+    var inngang = L.marker([60, 317],{icon: inngangIcon}).bindPopup("Inngang").addTo(map);
+    var kasse = L.marker([60, 204],{icon: kasseIcon}).bindPopup("Kasse").addTo(map);
+
 
 };
 
