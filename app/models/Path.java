d33e4171f10195d6ecb9bab7e8fc8686919c82f3
gib2 <mathilde.orstavik@gmail.com>
2015-04-17 15:13:43 +0200

    models

--- app/models/Path.java
+++ app/models/Path.java
@@ -15,6 +15,8 @@ public class Path{
     public static List<Vertex>finalPath;
     public static weightedGraph wGraph;
 
+    public static List<Vertex> closeToEnd;
+    public static List<Vertex> closeToStart;
 
     public Path(Vertex root, Vertex end){
         //finner en graf som innkluderer kun targetnodene
@@ -29,17 +31,72 @@ public class Path{
         targets.remove(Vertex.find.byId(1));
         targets.remove(Vertex.find.byId(18));
 
-        //Finner alle mulige måter å travesere grafen på
-        AllPermutations allPerm=new AllPermutations(targets, Vertex.find.byId(1), Vertex.find.byId(18), wGraph);
+        boolean big;
+        if(targets.size()>7) {
+            big = true;
+            ArrayList<Vertex> visited=new ArrayList<>();
+            closeToStart=new ArrayList<>();
+            closeToEnd=new ArrayList<>();
 
-        for(List<Vertex> list:allPerm.allPermutations){
+            //finner de to nærmeste nodene til startnoden
+            visited.add(root);
+            Vertex closestToRoot=closestTo(root, visited);
+            closeToStart.add(closestToRoot);
+            visited.add(closestToRoot);
+
+            Vertex closestNext=closestTo(closestToRoot,visited);
+            closeToStart.add(closestNext);
+
+            //Finner de to nærmeste nodene til kassenoden
+            Vertex closestToEnd=closestTo(end, visited);
+            closeToEnd.add(closestToEnd);
+            visited.add(closestToEnd);
+
+            closestNext=closestTo(closestToEnd,visited);
+            closeToEnd.add(closestNext);
+
+            //Fjerner de fra targets for å minske antall permutasjoner
+            targets.removeAll(closeToEnd);
+            targets.removeAll(closeToStart);
+        }
+        
+
+        else{
+            big=false;
+
+        }
+        //Finner alle mulige måter å travesere grafen på
+        AllPermutations allPerm = new AllPermutations(targets, Vertex.find.byId(1), Vertex.find.byId(18), wGraph);
+
+        for (List<Vertex> list : allPerm.allPermutations) {
             System.out.println("perm: ");
-            for(Vertex v:list){
+            for (Vertex v : list) {
                 System.out.println(v.id);
             }
         }
+
         //Finner beste sti ved å si hvilken som totalt sett er kortest
-        finalPath=allPerm.getBestPath();
+        finalPath = allPerm.getBestPath(big);
+
+    }
+
+    public static Vertex closestTo(Vertex vertex, ArrayList<Vertex> visited){
+
+        int w=99999;
+        Vertex closest=Vertex.find.byId(1);
+
+        for (weightedEdge edge : wGraph.edges){
+            if (edge.getSource().equals(vertex)&& (edge.weight<w) && (!visited.contains(edge.getDestination())) ){
+                closest=edge.getDestination();
+                w=edge.weight;
+            }
+            if(edge.getDestination().equals(vertex) && (edge.weight<w) && (!visited.contains(edge.getSource())) ) {
+                closest= edge.getSource();
+                w=edge.weight;
+            }
+        }
+
+        return (closest);
 
     }
 
